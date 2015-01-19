53cfefa8477460808cb26f6ed471188d1a764975
Harold Putman <hputman@lexmark.com>
2015-01-19 11:05:59 -0500

    Add note about ruby and homebrew.

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,4 +1,6 @@
 # init according to man page
+# Note: Don't install ruby with homebrew if you are using rbenv.
+export RBENV_ROOT=/usr/local/var/rbenv
 if (( $+commands[rbenv] ))
 then
   eval "$(rbenv init -)"
