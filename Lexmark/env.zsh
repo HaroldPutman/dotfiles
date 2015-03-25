45f82dfd4f0ade6348ee3916fa346037e83a7f90
Harold Putman <hputman@lexmark.com>
2015-03-24 21:28:58 -0400

    Add support for bower in Lexmark Proxy
    Uses the bower_proxy and bower_https_proxy environment variables.

--- Lexmark/env.zsh
+++ Lexmark/env.zsh
@@ -3,4 +3,6 @@ if [ $? -eq 0 ]; then
   echo "at Lexmark"	
   export http_proxy=${http_proxy:='http://us-auto.proxy.lexmark.com:8080'}
   export no_proxy=${no_proxy:='localhost,127.0.0.1,*.lexmark.com,*.local'}
+  export bower_proxy=${bower_proxy:='http://us-auto.proxy.lexmark.com:8080'}
+  export bower_https_proxy=${bower_https_proxy:='false'}
 fi
