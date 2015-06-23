466a92993a6c0cf31e99978a5caac0a5121aee81
Harold Putman <hputman@lexmark.com>
2015-06-23 11:58:55 -0400

    Add npm proxy
    Setup the proxy for NPM while inside Lexmark firewall

--- Lexmark/env.zsh
+++ Lexmark/env.zsh
@@ -5,4 +5,6 @@ if [ $? -eq 0 ]; then
   export no_proxy=${no_proxy:='localhost,127.0.0.1,*.lexmark.com,*.local'}
   export bower_proxy=${bower_proxy:='http://us-auto.proxy.lexmark.com:8080'}
   export bower_https_proxy=${bower_https_proxy:=''}
+  export npm_config_proxy=${npm_config_proxy:='http://us-auto.proxy.lexmark.com:8080'}
+  export npm_config_https_proxy=${npm_config_https_proxy:=''}
 fi
