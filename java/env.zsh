472560caf59e4231bff391628dc94e367d1feba1
Harold Putman <hputman@lexmark.com>
2015-08-05 10:01:11 -0400

    Use jenv

--- java/env.zsh
+++ java/env.zsh
@@ -1,1 +1,3 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home
+export PATH=$HOME/.jenv/bin:$PATH
+if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+# export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home
