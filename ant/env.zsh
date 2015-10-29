6b314d6e7112624da2dc171b84d4a07ef0ae6359
Harold Putman <hputman@lexmark.com>
2015-10-29 13:48:48 -0400

    Cleanup

--- ant/env.zsh
+++ ant/env.zsh
@@ -1,2 +1,2 @@
-export ANT_HOME="/usr/local/Cellar/ant/1.9.4/libexec"
+export ANT_HOME="/usr/local/Cellar/ant/1.9.6/libexec"
 export ANT_OPTS="-Xmx2048m -Xms512m -Dhttp.proxyHost=cache1.lexmark.com -Dhttp.proxyPort=80"
